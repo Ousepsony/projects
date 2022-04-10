b9d0b2fceead721c351b54fd97a66e298037a261
Ousepsony <ousepsony@gmail.com>
2022-04-10 14:35:04 +0530

    added support libs

---
+++ calculator/script.js
@@ -0,0 +1,90 @@
+// var calcDisplay = document.getElementById("CalcDisplay")
+
+// var btn1 =document.getElementById("CalcButton1");
+// var btn2 =document.getElementById("CalcButton2");
+// var btn3 =document.getElementById("CalcButton3");
+// var btn4 =document.getElementById("CalcButton4");
+// var btn5 =document.getElementById("CalcButton5");
+// var btn6 =document.getElementById("CalcButton6");
+// var btn7 =document.getElementById("CalcButton7");
+// var btn8 =document.getElementById("CalcButton8");
+// var btn9 =document.getElementById("CalcButton9");
+// var btn0 =document.getElementById("CalcButton0");
+// var btnAdd =document.getElementById("CalcButtonAdd");
+// var btnSub =document.getElementById("CalcButtonSub");
+// var btnMul =document.getElementById("CalcButtonMultply");
+// var btnDiv =document.getElementById("CalcButtonDivide");
+// var btnEqual =document.getElementById("CalcButtonEqual");
+
+// document.addEventListener("click",function textInput(){
+//     console.log("clicked");
+//     // calcDisplay.innerHTML += this.innerHTML;
+// });
+
+// document.addEventListener('keydown', function (event) {
+//     console.log("keydown")
+//     x=calcDisplay.innerHTML;
+//     if (x.length < 5){
+        
+    
+//     if (event.key == 1) {
+//       calcDisplay.innerHTML +=1;
+//     }
+//     else if(event.key == 2) {
+//         calcDisplay.innerHTML +=2;
+//       }
+//     else if(event.key == 3) {
+//         calcDisplay.innerHTML +=3;
+//     }
+//     else if(event.key == 4) {
+//         calcDisplay.innerHTML +=4;
+//     }
+//     else if(event.key == 5) {
+//         calcDisplay.innerHTML +=5;
+//     }
+//     else if(event.key == 6) {
+//         calcDisplay.innerHTML +=6;
+//     }
+
+
+
+
+
+
+//     else if 
+//     ((event.key !== 1) 
+//     || (event.key !== 2) 
+//     || (event.key !== 3) 
+//     || (event.key !== 4)
+//     || (event.key !== 5)
+//     || (event.key !== 6) 
+//     || (event.key !== 7)
+//     || (event.key !== 8)
+//     || (event.key !== 9)
+//     || (event.key !== 0)
+//     || (event.key !== "+")
+//     || (event.key !== "-")
+//     || (event.key !== "*")
+//     || (event.key !== "/")
+//     || (event.key !== "Backspace")
+//     || (event.key !== "Enter")
+//     || (event.key !== "NumpadDecimal")
+//     ) {
+//         console.log("other key down")
+//       }  
+//     else{
+//         console.log("real key down")
+//     }  
+// }
+// else{
+//     console.log("limit exceeded")
+// }
+//   });
+
+// window.addEventListener("resize", function() {
+//     console.log("resized");
+//   })
+
+
+
+console.log(eval("123+234*123"));
