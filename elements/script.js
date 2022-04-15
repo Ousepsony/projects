237bc22b871a58e1afdee709f9c5a078d435a318
Ousepsony <ousepsony@gmail.com>
2022-04-15 21:43:11 +0530

    random generator seem sto be working fine

--- elements/script.js
+++ elements/script.js
@@ -1,24 +1,16 @@
-// // document.querySelector("body").addEventListener("click",createElements())
 
-// document.onclick = createElements();
-// function createElements(){
-//     var randNumber = Math.floor(Math.random() * 5);
-//     console.log(randNumber)
-// }
-var windowHeight =  window.innerHeight;
-document.getElementById("bodyDiv").style.height = windowHeight+"px";
 var randBgColor = Math.floor(Math.random()*255)
-document.getElementById("bodyDiv").style.backgroundColor = "rgb("+randBgColor +", "+ randBgColor+", "+randBgColor+ ")";
+document.body.style.backgroundColor = "rgb("+randBgColor +", "+ randBgColor+", "+randBgColor+ ")";
 
 
-//document.getElementById("bodyDiv").addEventListener("click", myFunction);
+window.addEventListener("click", createElements);
 
+function createElements(){
+  console.log("clicked");
+  this.newElement = document.createElement("div");
+  this.newElement.innerHTML = "";
+  this.newElement.setAttribute("class","elements")
 
+  document.body.appendChild(this.newElement);
+}
 
-//function myFunction() {
-  //var randomElements = Math.floor(Math.random()*3);
-  //for(i=0;i<3;i++){
-      //const para= document.createElement("p");
-      //para.innerHTML = "this is a para";
-  //}
-//}
