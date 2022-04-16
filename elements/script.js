d15a1ca386603801b899422537395ad4877fe04a
Ousepsony <ousepsony@gmail.com>
2022-04-16 10:47:29 +0530

    change

--- elements/script.js
+++ elements/script.js
@@ -1,27 +1,40 @@
-
-var randBgColor = Math.floor(Math.random()*255)
-document.body.style.backgroundColor = "rgb("+Math.floor(Math.random()*255) +", "+Math.floor(Math.random()*255)+", "+Math.floor(Math.random()*255)+ ")";
+var RandBg= "hsla("+0 +", "+0+"%, "+Math.floor(Math.random() * (80) + 79)+ "%,1)";
+document.body.style.backgroundColor = RandBg;
 
 
 window.addEventListener("click", createElements);
-
+ var counter = 0;
 function createElements(){
-  console.log("clicked");
-  this.newElement = document.createElement("div");
-  this.newElement.innerHTML = "";
+  if(counter<10){
+    var clickRandBg = "hsla("+0 +", "+0+"%, "+Math.floor(Math.random() * (80) + 79)+ "%,1)";
+  document.body.style.backgroundColor = clickRandBg;
 
-  this.newElement.setAttribute("class","elements");
+  
+  var number = Math.floor(Math.random() * 3)
+  for(var i = 0;i<= number;i++){
+    this.newElement = document.createElement("div");
+    this.newElement.innerHTML = "";
+  
+    this.newElement.setAttribute("class","elements");
+  
+    var randomSeed = Math.floor(Math.random() * (window.innerHeight))
+  
+    this.newElement.style.minHeight =randomSeed /3 + "px";
+    this.newElement.style.maxHeight =randomSeed /3 + "px";
+    this.newElement.style.minWidth =randomSeed /3 + "px";
+    this.newElement.style.minWidth =randomSeed /3+ "px";
+  
+    this.newElement.style.backgroundColor = "hsla("+ Math.floor(Math.random() * 48) +","+ 100 +"%,"+50  +"%,"+ (Math.random() * 0.90 + 0.60) +")";
+    console.log("Element Properties: color:" + this.newElement.style.backgroundColor) 
+    this.newElement.style.top = Math.floor(Math.random() * (window.innerHeight - randomSeed)) + "px";
+    this.newElement.style.left = Math.floor(Math.random() * (window.innerHeight)) + "px";
 
-  var randomSeed = Math.floor(Math.random() * (window.innerHeight / 2 ))
-  this.newElement.style.minHeight =randomSeed + "px";
-  this.newElement.style.maxHeight =randomSeed + "px";
-  this.newElement.style.minWidth =randomSeed + "px";
-  this.newElement.style.minWidth =randomSeed + "px";
-  this.newElement.style.backgroundColor = "rgb("  + (255 - randomSeed) +","+(255 - randomSeed /3) +","+(255 - randomSeed /5)+ ")";
-  this.newElement.style.top = Math.floor(Math.random() * (window.innerHeight - randomSeed)) + "px";
-  this.newElement.style.left = Math.floor(Math.random() * (window.innerHeight)) + "px";
-
-
-  document.body.appendChild(this.newElement);
+    
+  
+    document.body.appendChild(this.newElement);
+  }
+  counter+=1;
+  console.log(counter)
 }
-
+  
+}
