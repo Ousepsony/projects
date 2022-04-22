288d1a82b3a0e15d19f1004cd241527f2173d12e
Ousepsony <ousepsony@gmail.com>
2022-04-22 14:40:00 +0530

    ui changes;

---
+++ neumorphism/index.js
@@ -0,0 +1,36 @@
+var tile = document.getElementById("tile");
+var dot = document.getElementById("dot");
+
+dot.style.top = window.innerHeight/2 - 12.5  + "px";
+dot.style.left = window.innerWidth/2 - 12.5  + "px";
+
+tile.style.top = window.innerHeight/2 - 150  + "px";
+tile.style.left = window.innerWidth/2 - 150  + "px";
+
+window.addEventListener("resize",resizeElements);
+function resizeElements(){
+    tile.style.top = window.innerHeight/2 - 150  + "px";
+    tile.style.left = window.innerWidth/2 - 150  + "px";   
+}
+
+window.addEventListener("mousemove",mouseMoveChange);
+function mouseMoveChange(event){
+    var mouseX =  event.clientX;
+    var mouseY = event.clientY;
+    dot.style.top = mouseY-12 + "px";
+    dot.style.left = mouseX-12 + "px";
+
+    var styleMouseX = mouseX/2;
+    var styleMouseY = mouseY/2;
+    
+    var ww = window.innerWidth;
+    var wh = window.innerHeight;
+
+    if(styleMouseX<(ww/2)){
+        styleMouseX =  (-(styleMouseX));
+    }
+    if(styleMouseY<(wh/2)){
+        styleMouseY =  (-(styleMouseY));
+    }
+    console.log(styleMouseX,styleMouseY)
+}
