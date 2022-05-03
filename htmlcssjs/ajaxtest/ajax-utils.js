53ad6213d7e20eaee2cc68ec1a40b0d1b145df8b
Ousepsony <ousepsony@gmail.com>
2022-05-03 20:08:30 +0530

    updated ajax to ajax-v2

--- htmlcssjs/ajaxtest/ajax-utils.js
+++ htmlcssjs/ajaxtest/ajax-utils.js
@@ -22,32 +22,46 @@
   
   // Makes an Ajax GET request to 'requestUrl'
   ajaxUtils.sendGetRequest = 
-    function(requestUrl, responseHandler) {
+    function(requestUrl, responseHandler, isJsonResponse) {
       var request = getRequestObject();
       request.onreadystatechange = 
         function() { 
-          handleResponse(request, responseHandler); 
+          handleResponse(request, 
+                         responseHandler,
+                         isJsonResponse); 
         };
       request.open("GET", requestUrl, true);
       request.send(null); // for POST only
     };
   
-  
+
   // Only calls user provided 'responseHandler'
   // function if response is ready
   // and not an error
   function handleResponse(request,
-                          responseHandler) {
+                          responseHandler,
+                          isJsonResponse) {
     if ((request.readyState == 4) &&
        (request.status == 200)) {
-      responseHandler(request);
+  
+      // Default to isJsonResponse = true
+      if (isJsonResponse == undefined) {
+        isJsonResponse = true;
+      }
+  
+      if (isJsonResponse) {
+        responseHandler(JSON.parse(request.responseText));
+      }
+      else {
+        responseHandler(request.responseText);
+      }
     }
   }
   
   
   // Expose utility to the global object
   global.$ajaxUtils = ajaxUtils;
-  
+
   
   })(window);
   
