ae6140421917fc35fe74badeb55baed055ac8462
Ousepsony <ousepsony@gmail.com>
2022-05-04 22:18:54 +0530

    increased ajax efficency (parses JSON as function)

--- htmlcssjs/ajaxtest2/ajax.js
+++ htmlcssjs/ajaxtest2/ajax.js
@@ -5,15 +5,19 @@ var self = this;
 
 btn1.addEventListener("click",loadBtn1)
 
-var loadBtn1 = function(){
+function loadBtn1(){
     var xhr = new XMLHttpRequest();
-    xhr.open("GET","/data/data.txt",true);
+    xhr.open("GET","data/data.txt",true);
 
 
     xhr.onload = function () {
         if(this.status == 200){
             var text = this.responseText;
-            console.log(this.responseText)
+            console.log(text)
+
+            document.getElementsByClassName("content").innerHTML = "<h1>" + text + "</h1>";
         }
     }
+
+    xhr.send();
 }
