0941e548b7be9e4d0187baf1bf7a8e25ede741fa
Ousepsony <ousepsony@gmail.com>
2022-05-18 21:02:57 +0530

    changes on elements.js

--- neumorphism/elements.js
+++ neumorphism/elements.js
@@ -3,3 +3,9 @@ dot.style.left = window.innerWidth/2 - 12.5  + "px";
 
 tile.style.top = window.innerHeight/2 - 150  + "px";
 tile.style.left = window.innerWidth/2 - 150  + "px";
+
+
+
+var tile = document.getElementById("tile");
+var dot = document.getElementById("dot");
+var header = document.getElementById("header");
