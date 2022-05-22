61020622ecd29ebece10f48e5c31b15755bebab7
Ousepsony <100663601+Ousepsony@users.noreply.github.com>
2022-05-22 18:51:55 +0530

    Update index.js

--- neumorphism/index.js
+++ neumorphism/index.js
@@ -1,44 +1,51 @@
 var globalScope = {};
+
 globalScope.counter = 0;
 globalScope.isInset = false;
 
 
-globalScope.insetText;
+globalScope.insetText="";
 
 
-function applyStyles(className,insetValue,mouseX, mouseY,dividend){
-    
-    var tileBS =globalScope.insetText+ (mouseX/dividend) + "px " + (mouseY/dividend) + "px "+ "60"+"px #ffffff";
-    var tileBS2 =globalScope.insetText+  -1*((mouseY/dividend)) + "px " + -1*((mouseY/dividend)) + "px "+ "60"+"px #bebebe";
 
-    var tileBS =globalScope.insetText + (mouseX / dividend) + "px " + (mouseY/dividend) + "px "+ "60"+"px #ffffff";
-    var tileBS2 =globalScope.insetText +  -1*(mouseX/dividend) + "px " + -1*(mouseY/dividend) + "px "+ "60"+"px #bebebe";
-
-
-    // console.warn(tileBS,tileBS2)
-    dot.style.top = mouseX-250 + "px";
-    dot.style.left = mouseY-250 + "px";
-    tile.style.boxShadow = tileBS+","+tileBS2;
-}
 
 window.addEventListener("mousemove",moven);
 function moven(event){
     globalScope.mouseX = event.clientX;
     globalScope.mouseY =  event.clientY;
-   applyStyles("element",globalScope.isInset,globalScope.mouseX,globalScope.mouseY,10)
+   applyStyles(globalScope.isInset,globalScope.mouseX,globalScope.mouseY,10);
+//    globalScope.insetText = globalScope.insetText;    
+//    console.log(globalScope.mouseX,globalScope.mouseY);
+   
 }
 
 window.addEventListener("click",clicked);
 function clicked(){
-    console.log(globalScope.mouseX,globalScope.mouseY);
 
-    if(globalScope.inset==true){
-        globalScope.insetText =  "inset "
-        globalScope.inset=false;
+    if(globalScope.counter % 2 === 1){
+        globalScope.insetText  = "inset ";
+        applyStyles(globalScope.isInset,globalScope.mouseX,globalScope.mouseY,10);
+        globalScope.counter +=1;
+    }
+    else if (globalScope.counter % 2 ===0){
+        globalScope.insetText ="";
+        applyStyles(globalScope.isInset,globalScope.mouseX,globalScope.mouseY,10);
+        globalScope.counter += 1;
+    }
+    
+}
 
-    }
-    else if(globalScope.inset ==  false){
-        globalScope.insetText="";
-        globalScope.inset = true;
-    }
+function applyStyles(insetValue,mouseX, mouseY,dividend){
+
+    var propX = mouseX-(window.innerWidth /2);
+    var propY = (mouseY-(window.innerHeight /2));    
+    var insideTag =  document.getElementsByClassName("element");
+
+    var tileBS =globalScope.insetText+ (propX/dividend) + "px " + (propY/dividend) + "px "+ "60"+"px #ffffff";
+    var tileBS2 =globalScope.insetText+  -1*((propX/dividend)) + "px " + -1*((propY/dividend)) + "px "+ "60"+"px #bebebe";
+    dot.style.top =mouseY - 250 + "px";
+    dot.style.left = mouseX - 250 + "px";
+    for(i=0; i<insideTag.length; i++) {
+        insideTag[i].style.boxShadow = tileBS+","+tileBS2;
+      }
 }
