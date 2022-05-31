beab3c5da2e320a15a7d5df0e7e749def08ec5c3
Ousepsony <ousepsony@gmail.com>
2022-05-31 15:35:34 +0530

    ts test

--- typescript-test/src/index.ts
+++ typescript-test/src/index.ts
@@ -1,4 +1,17 @@
-let age:number =20;
-if (age <50){
-    age+=10
-}
+function resolveAfter2Seconds() {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve('resolved');
+      }, 2000);
+    });
+  }
+  
+  async function asyncCall() {
+    console.log('calling');
+    const result = await resolveAfter2Seconds();
+    console.log(result);
+    // expected output: "resolved"
+  }
+  
+  asyncCall();
+  
