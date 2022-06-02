41a4990a7a2b46da72bc120edc84256782ef9f06
Ousepsony <ousepsony@gmail.com>
2022-06-02 15:54:04 +0530

    program that print fizz buzz question

--- js-test/script.js
+++ js-test/script.js
@@ -1,21 +1,47 @@
-function isLeap(year) {
+// function isLeap(year) {
     
-    /**************Don't change the code above****************/    
+//     /**************Don't change the code above****************/    
         
-        //Write your code here.    
-    if(year%4 === 0 && year%100!=0){
-        console.log("leap year");
-    }
-    else if (year %100&&year%400==0){
-        console.log("not leap")
+//         //Write your code here.    
+//     if(year%4 === 0 && year%100!=0){
+//         console.log("leap year");
+//     }
+//     else if (year %100&&year%400==0){
+//         console.log("not leap")
+//     }
+//     else{
+//         console.log("not leap")
+//     }
+    
+        
+    
+//     /**************Don't change the code below****************/    
+    
+//     }
+//     isLeap(2020)
+
+
+//arrays
+// var array1 = ["one","two","three","four","five"];
+// var i=0
+// for (i = 0; i < array1.length; i++){
+//   console.log((i+1) + ": " + array1[i]);}
+i=1;
+
+while (i<=100){
+    if(i%3==0 || i%5==0){
+        if (i%3==0 && i%5==0){
+            console.log("fizzBuzz")
+        }
+        else if(i%3==0){
+            console.log("fizz")
+        }
+        else if(i%5==0){
+            console.log("Buzz")
+        }
     }
     else{
-        console.log("not leap")
+        console.log(i)
     }
-    
-        
-    
-    /**************Don't change the code below****************/    
-    
-    }
-    isLeap(2020)
+    i++
+}
