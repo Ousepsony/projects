ac2a66374effc539c07a84ab36295ece4201953e
Ousepsony <ousepsony@gmail.com>
2022-06-02 15:54:26 +0530

    changes on index.js

--- js-test/script.js
+++ js-test/script.js
@@ -1,31 +1,5 @@
-// function isLeap(year) {
-    
-//     /**************Don't change the code above****************/    
-        
-//         //Write your code here.    
-//     if(year%4 === 0 && year%100!=0){
-//         console.log("leap year");
-//     }
-//     else if (year %100&&year%400==0){
-//         console.log("not leap")
-//     }
-//     else{
-//         console.log("not leap")
-//     }
-    
-        
-    
-//     /**************Don't change the code below****************/    
-    
-//     }
-//     isLeap(2020)
 
-
-//arrays
-// var array1 = ["one","two","three","four","five"];
-// var i=0
-// for (i = 0; i < array1.length; i++){
-//   console.log((i+1) + ": " + array1[i]);}
+": " + array1[i]);}
 i=1;
 
 while (i<=100){
