6766077a9e395f7514c2e36a7bf6c26b94ba66a4
Ousepsony <ousepsony@gmail.com>
2022-06-02 15:31:23 +0530

    leapyear Test

--- js-test/script.js
+++ js-test/script.js
@@ -1,2 +1,21 @@
-//index.js
-console.log(Math.floor(Math.random()*122))
+function isLeap(year) {
+    
+    /**************Don't change the code above****************/    
+        
+        //Write your code here.    
+    if(year%4 === 0 && year%100!=0){
+        console.log("leap year");
+    }
+    else if (year %100&&year%400==0){
+        console.log("not leap")
+    }
+    else{
+        console.log("not leap")
+    }
+    
+        
+    
+    /**************Don't change the code below****************/    
+    
+    }
+    isLeap(2020)
