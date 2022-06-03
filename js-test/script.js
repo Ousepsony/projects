fbde642ab88ad6593acf50b88e594b0bda512681
Ousepsony <ousepsony@gmail.com>
2022-06-03 15:19:38 +0530

    cleared sriipt.js (1_

--- js-test/script.js
+++ js-test/script.js
@@ -1,20 +1,20 @@
 
-i=1;
+// i=1;
 
-while (i<=100){
-    if(i%3==0 || i%5==0){
-        if (i%3==0 && i%5==0){
-            console.log("fizzBuzz")
-        }
-        else if(i%3==0){
-            console.log("fizz")
-        }
-        else if(i%5==0){
-            console.log("Buzz")
-        }
-    }
-    else{
-        console.log(i)
-    }
-    i++
-}
+// while (i<=100){
+//     if(i%3==0 || i%5==0){
+//         if (i%3==0 && i%5==0){
+//             console.log("fizzBuzz")
+//         }
+//         else if(i%3==0){
+//             console.log("fizz")
+//         }
+//         else if(i%5==0){
+//             console.log("Buzz")
+//         }
+//     }
+//     else{
+//         console.log(i)
+//     }
+//     i++;
+// }
