33dd59e6e7e9dd6680b5fad03d7c750f006b19ba
Ousepsony <ousepsony@gmail.com>
2022-06-03 15:26:06 +0530

    changes on index(1).js

--- js-test/script.js
+++ js-test/script.js
@@ -1,20 +1,14 @@
-
-// i=1;
-
-// while (i<=100){
-//     if(i%3==0 || i%5==0){
-//         if (i%3==0 && i%5==0){
-//             console.log("fizzBuzz")
-//         }
-//         else if(i%3==0){
-//             console.log("fizz")
-//         }
-//         else if(i%5==0){
-//             console.log("Buzz")
-//         }
-//     }
-//     else{
-//         console.log(i)
-//     }
-//     i++;
-// }
+for(i =1;i<=100;i++){
+    if (i%3==0 && i%5==0){
+        console.log("fizzBuzz")
+    }
+    else if(i%3==0){
+        console.log("fizz")
+    }
+    else if(i%5==0){
+        console.log("Buzz")
+    }
+    else{
+    console.log(i)
+    }
+}
