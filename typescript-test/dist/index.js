e2afb5fd31a44f118af00564a3401f6a838e9d7d
Ousepsony <ousepsony@gmail.com>
2022-06-05 12:34:10 +0530

    tests on typescript

--- typescript-test/dist/index.js
+++ typescript-test/dist/index.js
@@ -1,2 +1,9 @@
 "use strict";
+class UserAccount {
+    constructor(name, id) {
+        this.name = name;
+        this.id = id;
+    }
+}
+const user = new UserAccount("Murphy", 1);
 //# sourceMappingURL=index.js.map
