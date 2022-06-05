6a9dad8775dfd9f3f83eec0d5e7c1c6d200ba090
Ousepsony <ousepsony@gmail.com>
2022-06-05 12:35:02 +0530

    completed tests on typescript-js environment

--- typescript-test/dist/index.js
+++ typescript-test/dist/index.js
@@ -1,9 +1,2 @@
 "use strict";
-class UserAccount {
-    constructor(name, id) {
-        this.name = name;
-        this.id = id;
-    }
-}
-const user = new UserAccount("Murphy", 1);
 //# sourceMappingURL=index.js.map
