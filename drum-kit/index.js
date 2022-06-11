2955ee28fc59708a4d6f09c3ee5e40cd85adbac5
Ousepsony <ousepsony@gmail.com>
2022-06-11 17:05:43 +0530

    drum-kit changes

--- drum-kit/index.js
+++ drum-kit/index.js
@@ -1,24 +1,9 @@
-var gameState =0;
-var score = 0;
-var globalScope ={};
-
-
-window.setInterval(getMouseCoords(),1000)
-function getMouseCoords (event){
-    globalScope.clientX = event.clientX;
-    globalScope.clientY = event.clientY;
-};
-
-console.log(globalScope.clientX,globalScope.clientY)
-if(gameState ===0){
-    // var element = 
-    // document.createElement("button");
-    // element.innerHTML = "test";
-    // document.body.appendChild(element)
-    
+x= document.querySelectorAll(".drum").length;
+for (i in document.querySelectorAll(".drum").length){
+    console.log(document.querySelectorAll(".drum")[i])
+    document.querySelectorAll(".drum")[i].addEventListener("click",clickFunction);
 }
 
-if(gameState === 1){
-    
+var clickFunction = function(){
+    console.log("click");
 }
-
