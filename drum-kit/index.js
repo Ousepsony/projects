6c34e2e277d14edec543811ee1ccbf006531b8c9
Ousepsony <ousepsony@gmail.com>
2022-06-14 13:37:45 +0530

    better "addEventListener" loop

--- drum-kit/index.js
+++ drum-kit/index.js
@@ -2,6 +2,7 @@ for(let i = 0; i<=document.querySelectorAll(".drum").length; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",clicked);
 }
 
+
 function clicked(){
     console.log("clicked")
 }
