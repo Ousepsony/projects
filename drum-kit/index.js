d2007ffd466f93c3949c4f60ea1621c08a39a8cd
Ousepsony <ousepsony@gmail.com>
2022-06-14 13:37:15 +0530

    for let loop working now on adding event listener

--- drum-kit/index.js
+++ drum-kit/index.js
@@ -1,4 +1,4 @@
-for(let i = 0; i<=document.querySelectorAll(".drum").length;i++){
+for(let i = 0; i<=document.querySelectorAll(".drum").length; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",clicked);
 }
 
