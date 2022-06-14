95a878901ea7aa2070afda0305269bde0e208922
Ousepsony <ousepsony@gmail.com>
2022-06-14 13:28:03 +0530

    changes on index.js

--- drum-kit/index.js
+++ drum-kit/index.js
@@ -1,5 +1,5 @@
 for(let i = 0; i<=document.querySelectorAll(".drum").length;i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click",clicked)
+    document.querySelectorAll(".drum")[i].addEventListener("click",clicked);
 }
 
 function clicked(){
