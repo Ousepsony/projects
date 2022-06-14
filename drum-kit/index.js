9d9be353db4cff269edaa1bf5d7114c4d5b9f6e5
Ousepsony <ousepsony@gmail.com>
2022-06-14 14:04:10 +0530

    higher order functions test

--- drum-kit/index.js
+++ drum-kit/index.js
@@ -1,7 +1,27 @@
-for(let i = 0; i<=document.querySelectorAll(".drum").length; i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click",clicked);
+// for(let i = 0; i<=document.querySelectorAll(".drum").length; i++){
+//     document.querySelectorAll(".drum")[i].addEventListener("click",clicked);
+// }
+
+// function clicked(){
+//     console.log("clicked")
+// }
+
+//higher order fucntion test
+
+function add(x,y){
+    
+    return x+y
+}
+function subtract(x,y){
+    return x-y
+}
+function multiply(x,y){
+    return x*y
+}
+function divide(x,y){
+    return x/y
 }
 
-function clicked(){
-    console.log("clicked")
+function calculator(num1,num2, operation){
+    return(operation(num1,num2))
 }
