2972685cf191985220ec3c3e42121f477ed48ba2
Ousepsony <ousepsony@gmail.com>
2022-06-17 16:37:02 +0530

    changes on index.js ( adding anime.js)

--- drum-kit/index.js
+++ drum-kit/index.js
@@ -5,6 +5,7 @@ for(let i = 0; i<document.querySelectorAll(".drum").length; i++){
     document.querySelectorAll(".drum")[i].addEventListener("keyup",pressed);;
 }
 function clicked(event) {
+    
     var audio = new Audio("sounds/"+this.name+".mp3");
     audio.play();
     this.style.animate()
