08626b72feb39c523863faf13be01d9d47215d58
Ousepsony <ousepsony@gmail.com>
2022-06-17 16:44:34 +0530

    changes on js-test

--- js-test/script3.js
+++ js-test/script3.js
@@ -3,7 +3,10 @@ function BellBoy(name, age, salary, experience){
     this.age = age;
     this.salary = salary;
     this.experience =  experience;
+    this.welcome = function(){
+        console.log(`welcome ${name} `)
+    }
 }
 
 var bellBoy1 = new BellBoy("Joseph",23,12314,5)
-console.log(bellBoy1)
+bellBoy1.welcome();
