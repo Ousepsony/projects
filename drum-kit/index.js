09b5a7d47d7a4f231c8f74640f5ee984cdcc8e37
Ousepsony <ousepsony@gmail.com>
2022-06-18 11:38:01 +0530

    changes on index.js

--- drum-kit/index.js
+++ drum-kit/index.js
@@ -1,11 +1,44 @@
 for(let i = 0; i<document.querySelectorAll(".drum").length; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",clicked);
 }
-for(let i = 0; i<document.querySelectorAll(".drum").length; i++){
-    document.querySelectorAll(".drum")[i].addEventListener("keyup",pressed);;
-}
 function clicked(event) {
     var audio = new Audio("sounds/"+this.name+".mp3");
     audio.play();
-    this.style.animate()
+    // this.style.animate()
 };
+
+window.addEventListener("keyup",function(event){
+    switch (event.key) {
+        case "w":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;           
+            
+        default:
+            break;
+    }
+})
