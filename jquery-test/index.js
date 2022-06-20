192691933c1007b7ecc74e2e66ce1fd0386ed201
Ousepsony <ousepsony@gmail.com>
2022-06-20 20:33:19 +0530

    index.js changes on jquery-test

--- jquery-test/index.js
+++ jquery-test/index.js
@@ -1,2 +1,15 @@
 $(".test").css("color","rgb(245,145,45)");
-console.log($(".test").hasClass("test"))
+$(".test").html("<i>hello</i>")
+
+console.log($("h1").attr("class"))
+
+$('h1').click(function(){
+    // $('h1').css("color","black")
+})
+
+$(window).keypress(function(event){
+    // $('h1').text(event.key)
+})
+$(window).on("keypress",function(){
+    $('h1').sildeDown();
+})
