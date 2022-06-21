7a57c6ab6292e9e3e6aa8060d2a68f5f6f3773ec
Ousepsony <ousepsony@gmail.com>
2022-06-21 21:18:12 +0530

    changes on gmae.js in simon-game

--- simon-game/game.js
+++ simon-game/game.js
@@ -1,24 +1,42 @@
+var buttonColors = ["red","blue","green","yellow"];
 var gamePattern = [];
 var userClickedPattern = []
+var isKeyPressed = false;
 
-function playSound(name){
-    var audio = new Audio("sounds/"+name+".mp3")
-    audio.play();
-}
 
 function nextSequence(){
     var randomNumber= Math.floor(Math.random()*4);
-    var buttonColors = ["red","blue","green","yellow"];
+    
     randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor)
     $("#"+randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
-    playSound(""+randomChosenColor);
+    playSound(randomChosenColor);
 }
 console.log(nextSequence())
 
 $('.btn').click(function(){
     var userChosenColor = this.id;
     userClickedPattern.push(userChosenColor)
-    console.log(userClickedPattern)
-    playSound(this.id)
+    console.log(userClickedPattern);
+    animatePress(this);
+    playSound(this.id);
 })
+
+
+
+
+
+
+
+
+//indirect functions
+function playSound(name){
+    var audio = new Audio("sounds/"+name+".mp3")
+    audio.play();
+}
+function animatePress(currentColor){
+    $(currentColor).addClass("pressed");
+    var timeout =setTimeout(function(){
+        $(currentColor).removeClass("pressed")
+    }, 100);
+}
