93f78c6342004b603e7c0ffd0456adc6bff464a0
Ousepsony <ousepsony@gmail.com>
2022-06-21 21:06:53 +0530

    jquery addition (simon-game)

--- jquery-test/index.js
+++ jquery-test/index.js
@@ -1,15 +1,7 @@
-$(".test").css("color","rgb(245,145,45)");
-$(".test").html("<i>hello</i>")
 
-console.log($("h1").attr("class"))
-
-$('h1').click(function(){
-    // $('h1').css("color","black")
-})
-
-$(window).keypress(function(event){
-    // $('h1').text(event.key)
-})
-$(window).on("keypress",function(){
-    $('h1').sildeDown();
-})
+$("button").on("click",(function(){
+    $('h1').slideUp().slideDown().animate({margin:"10%"});
+}))
+// $(window).keypress(function(event){
+//     $('h1').sildeToggle();
+// })
