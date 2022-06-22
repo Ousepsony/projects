ac803111f3a82d83a80bebfa22ed8c26d82c0067
Ousepsony <ousepsony@gmail.com>
2022-06-22 17:57:25 +0530

    changes on index.js

--- simon-game/game.js
+++ simon-game/game.js
@@ -49,6 +49,14 @@ function checkAnswer(currentLevel) {
     } else {
 
       console.log("wrong");
+      playSound("wrong");
+      $("body").addClass("game-over");
+      setTimeout(function(){
+        $("body").removeClass("game-over");
+      },500);
+      $("h1").text("Game Over, Press Any Key to Restart");
+      started=false;
+      level=0;
 
     }
 
