e29be3dc036cd7df7ab14ad7834c7df575c541b2
Ousepsony <ousepsony@gmail.com>
2022-06-22 17:50:01 +0530

    game.js changes (better structured)

--- simon-game/game.js
+++ simon-game/game.js
@@ -1,42 +1,60 @@
-var buttonColors = ["red","blue","green","yellow"];
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+
 var gamePattern = [];
-var userClickedPattern = []
-var isKeyPressed = false;
+var userClickedPattern = [];
 
+var started = false;
+var level = 0;
+$(document).keypress(function() {
+  if (!started) {
+    $("#level-title").text("Level " + level);
+    nextSequence();
+    started = true;
+  }
+});
 
-function nextSequence(){
-    var randomNumber= Math.floor(Math.random()*4);
-    
-    randomChosenColor = buttonColors[randomNumber];
-    gamePattern.push(randomChosenColor)
-    $("#"+randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
-    playSound(randomChosenColor);
+$(".btn").click(function() {
+
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+});
+
+function nextSequence() {
+  level++;
+  $("#level-title").text("Level " + level);
+
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
 }
-console.log(nextSequence())
 
-$('.btn').click(function(){
-    var userChosenColor = this.id;
-    userClickedPattern.push(userChosenColor)
-    console.log(userClickedPattern);
-    animatePress(this);
-    playSound(this.id);
-})
-
-
-
-
-
-
-
-
-//indirect functions
-function playSound(name){
-    var audio = new Audio("sounds/"+name+".mp3")
-    audio.play();
+function checkAnswer(){
+  if(gamePattern[level-1]===userClickedPattern[level-1]){
+    console.log("success");
+  }
+  else{
+    console.log("incorrect")
+  }
 }
-function animatePress(currentColor){
-    $(currentColor).addClass("pressed");
-    var timeout =setTimeout(function(){
-        $(currentColor).removeClass("pressed")
-    }, 100);
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+function test(){
+    console.log("test")
 }
