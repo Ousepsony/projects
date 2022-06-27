913dc4d559a08f42145996c958f0ad9620dc7ee0
Ousepsony <ousepsony@gmail.com>
2022-06-27 20:58:42 +0530

    removed tests-/ directories  moved under same hood

--- jquery-test/index.js
+++
@@ -1,7 +0,0 @@
-
-$("button").on("click",(function(){
-    $('h1').slideUp().slideDown().animate({margin:"10%"});
-}))
-// $(window).keypress(function(event){
-//     $('h1').sildeToggle();
-// })
--- js-test/script3.js
+++
@@ -1,9 +0,0 @@
-function addEventListenerCopy (type,action){
-    var eventProperties = {
-        ClientX:100,
-        ClientY:200,
-    }
-    if(type="keypress"){
-        action(eventProperties);
-    }
-}
