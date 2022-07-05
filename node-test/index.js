4604a163a73cdce4b3d42155bfcd44dc4273f5f0
Ousepsony <ousepsony@gmail.com>
2022-07-05 21:06:58 +0530

    push to origin (main-content) of node modules

--- node-test/index.js
+++
@@ -1,7 +0,0 @@
-//jshint esversion:6
-// const fs = require("fs")
-// fs.copyFileSync('test-1.txt','file-2.txt');
-const supervillains = require("supervillains");
-// const superheroes = require("supervillains");
-
-console.log(supervillains.random())
