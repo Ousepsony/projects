6a8de6203dd2c7d5e385521c725dd1343f99e558
Ousepsony <ousepsony@gmail.com>
2022-07-09 12:25:29 +0530

    changes on app.js

--- express-weather-app/app.js
+++ express-weather-app/app.js
@@ -6,6 +6,12 @@ app.get("/",function(req,res){
     const url = "https://api.openweathermap.org/data/2.5/weather?q=paris&units=metric&appid=76f8e7de2a38fb1b4eaf6e4eaada5903"
     https.get(url, function(response){
         console.log(response.statusCode)
+        response.on("data",function(data){
+            const weatherData = JSON.parse(data);
+            // console.log(weatherData);
+            const temp = weatherData.main.feels_like;
+            console.log(weatherData.weather[0].description)
+        })
     })
     res.send("server is on mate")
 })
